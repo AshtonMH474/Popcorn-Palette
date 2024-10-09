98763b63c6b08429f4ffe000d35d2f09a736f6e0
Ashton <ashtonhowa@gmail.com>
2024-10-08 20:38:49 -0400

    starting watchlist

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,14 +1,18 @@
 import { NavLink } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
-// import { useSelector } from "react-redux";
+import { FaPlus } from "react-icons/fa6";
+import { FaSearch } from "react-icons/fa";
+import { useSelector } from "react-redux";
 // import LoginFormModal from "../LoginFormModal";
 // import OpenModalButton from '../OpenModalButton';
+
 import logo from '../../Static/2938928.webp'
+import { useState } from "react";
 
 function Navigation() {
-  // const sessionUser = useSelector(state=> state.session.user);
-
+  const sessionUser = useSelector(state=> state.session.user);
+  const [movie,setMovie] = useState('')
 
   return (
     <>
@@ -25,8 +29,23 @@ function Navigation() {
         </div>
       </li>
 
+      <li className="displayFlex">
+        <div>
+          <input className="search" type="text" value={movie} placeholder="search movie..." onChange={(e) => setMovie(e.target.value)}/>
+          <FaSearch className="searchButton"/>
+        </div>
+       {sessionUser && ( <div className="watchLink">
+          <NavLink className='displayFlex noTextUnderline' to='/watchlist'>
+          <div className="white">WATCHLIST</div>
+          <FaPlus className="white"/>
+          </NavLink>
+        </div>)}
+      </li>
+
       <li className="moveRight50px  moveDown5px">
+        <div>
         <ProfileButton />
+        </div>
       </li>
     </ul>
     </div>
