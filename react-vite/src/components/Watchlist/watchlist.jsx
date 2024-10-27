7f721892ce4c4f17afd4e3737033e8d2c5e63292
Ashton <ashtonhowa@gmail.com>
2024-10-27 15:37:48 -0400

    fixing watchlist page error for render

--- react-vite/src/components/Watchlist/watchlist.jsx
+++ react-vite/src/components/Watchlist/watchlist.jsx
@@ -8,7 +8,7 @@ import { FaRegTrashAlt } from "react-icons/fa";
 import { IoEyeOutline } from "react-icons/io5";
 import { Navigate,  useNavigate } from 'react-router-dom';
 // import { NavLink } from 'react-router-dom'
-import { getMovieDetails, getMovies } from '../../redux/movies'
+import { getMovieDetails} from '../../redux/movies'
 
 function Watchlist(){
     const dispatch = useDispatch()
@@ -22,7 +22,7 @@ function Watchlist(){
 
     useEffect(() => {
         dispatch(getWatchlist())
-        dispatch(getMovies()) //grabs movies so it knows if user has review or not
+        // dispatch(getMovies()) //grabs movies so it knows if user has review or not
         if(watchlistArr.length)setWatchlist(watchlistArr.filter(movie => movie.watched == false))
     },[dispatch,watchlistArr.length])
 
